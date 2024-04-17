46762e33f0dfff909adeb50c3e8c507580c75f19
TiparpronS <96738451+SukanyaT2022@users.noreply.github.com>
2024-04-17 12:44:21 -0400

    add

--- index.js
+++ index.js
@@ -28,11 +28,11 @@ function readInfofromTextBox() {
 
   //then make input box blank
   //below make input box empty after user fill all input and click add contact
-document.getElementById('readID').value = " "
-document.getElementById('readFirstName').value = " "
-document.getElementById('readLastName').value = " "
-document.getElementById('readAddress').value = " "
-document.getElementById('readPhoneNo').value = " "
+document.getElementById('readID').value = ""
+document.getElementById('readFirstName').value = ""
+document.getElementById('readLastName').value = ""
+document.getElementById('readAddress').value = ""
+document.getElementById('readPhoneNo').value = ""
 }
 
 // when insert new contact - it displays on the page
@@ -96,8 +96,10 @@ objArray.splice(i,1)//delete 1 object at i
 break
     }
 }
+//after delete then we refresh
 displayContact()
 }
+
 // modal 
 
 // Get the modal
@@ -111,19 +113,20 @@ var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal 
 btn.onclick = function() {
+var flag = false // if not found id ahow not found
+
   modal.style.display = "block";
   var readSearchID = document.getElementById('searchID').value;
+  while (modal.firstChild) {
+    modal.removeChild(modal.firstChild);
+
+  }
 for(i=0;i<objArray.length;i++){
+  console.log(objArray[i].contactID)
     if(objArray[i].contactID == readSearchID){
-      
-        while (modal.firstChild) {
-          modal.removeChild(modal.firstChild);
-        }
-      
-   
+flag = true
          modal.setAttribute('class', 'contact'); //control individual small box use this class
-      
-      
+          
           //create id
           var p2 = document.createElement('p'); //make paragraph tag
           var text2 = document.createTextNode('ID:' + objArray[i].contactID);
@@ -159,8 +162,18 @@ for(i=0;i<objArray.length;i++){
           modal.appendChild(button); // put button on small modal box
       
         //   document.getElementById('contactList').appendChild(modal); //we add all info small box in line 26 mainbox
-        }
-        
+        }  
+    }
+    document.getElementById('searchID').value =''
+    if(flag==false){
+      modal.setAttribute('class', 'contact'); //control individual small box use this class
+          
+      var p2 = document.createElement('p'); //make paragraph tag
+      p2.setAttribute('id', 'tagetNotFoundText')
+      var text2 = document.createTextNode('Contact is not found');
+      p2.appendChild(text2); //put text line 29 to paragraph line 28
+      modal.appendChild(p2); // after that put paragraph on the white box modal
+  
     }
 }
 
