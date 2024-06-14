1811e9e0885a7603bb08396c9b8f955bcf8d5d06
TiparpronS <96738451+SukanyaT2022@users.noreply.github.com>
2024-06-14 08:49:40 -0400

    add

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function person(id, first, last, address, phone) {
   this.address = address;
   this.phone = phone;
 }
-var objArray = []; 
+var objArray = [];
 
 function readInfofromTextBox() {
   var storeID = document.getElementById('readID').value;
@@ -14,7 +14,7 @@ function readInfofromTextBox() {
   var storeLastName = document.getElementById('readLastName').value;
   var storeAddress = document.getElementById('readAddress').value;
   var storePhoneNo = document.getElementById('readPhoneNo').value;
-//create new object
+  //create new object
   var obj = new person(
     storeID,
     storeFirstName,
@@ -23,16 +23,16 @@ function readInfofromTextBox() {
     storePhoneNo
   );
   objArray.push(obj); //push obj to obj array
-//then display
+  //then display
   displayContact();
 
   //then make input box blank
   //below make input box empty after user fill all input and click add contact
-document.getElementById('readID').value = ""
-document.getElementById('readFirstName').value = ""
-document.getElementById('readLastName').value = ""
-document.getElementById('readAddress').value = ""
-document.getElementById('readPhoneNo').value = ""
+  document.getElementById('readID').value = '';
+  document.getElementById('readFirstName').value = '';
+  document.getElementById('readLastName').value = '';
+  document.getElementById('readAddress').value = '';
+  document.getElementById('readPhoneNo').value = '';
 }
 
 // when insert new contact - it displays on the page
@@ -48,7 +48,6 @@ function displayContact() {
     var div = document.createElement('div');
     div.setAttribute('class', 'contact'); //control individual small box use this class
 
-
     //create id
     var p2 = document.createElement('p'); //make paragraph tag
     var text2 = document.createTextNode('ID: ' + objArray[i].contactID);
@@ -62,7 +61,6 @@ function displayContact() {
     );
     p1.appendChild(text); //put text line 29 to paragraph line 28
     div.appendChild(p1); // after that put paragraph on the white box div
-
 
     //create address
     var p3 = document.createElement('p'); //make paragraph tag
@@ -80,7 +78,10 @@ function displayContact() {
     button.setAttribute('type', 'button');
     button.setAttribute('value', 'Delete');
     button.setAttribute('class', 'btnDelete');
-    button.setAttribute('onclick','deleteObjButton('+objArray[i].contactID + ')')
+    button.setAttribute(
+      'onclick',
+      'deleteObjButton(' + objArray[i].contactID + ')'
+    );
     div.appendChild(button); // put button on small div box
 
     document.getElementById('contactList').appendChild(div); //we add all info small box in line 26 mainbox
@@ -89,127 +90,134 @@ function displayContact() {
 
 //function delete button link in line81
 
-function deleteObjButton(id){
-for (i=0;i<objArray.length;i++){
-    if(objArray[i].contactID==id){
-objArray.splice(i,1)//delete 1 object at i
-break
+function deleteObjButton(id) {
+  for (i = 0; i < objArray.length; i++) {
+    if (objArray[i].contactID == id) {
+      objArray.splice(i, 1); //delete 1 object at i
+      break;
     }
-}
-//after delete then we refresh
-displayContact()
+  }
+  //after delete then we refresh
+  displayContact();
 }
 
-// modal 
+// modal
 
 // Get the modal
-var modal = document.getElementById("myModal");
+var modal = document.getElementById('myModal');
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
+// Get the button submit ID
+var btn = document.getElementById('myBtn');
+// Get the button refresh
+var refeshBtn = document.getElementById('refresh');
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var span = document.getElementsByClassName('close')[0];
 
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-var flag = false // if not found id ahow not found
 
-  modal.style.display = "block";
+//refresh button
+refeshBtn.onclick = function () {
+  var flag = false; // if not found id ahow not found
+  modal.style.display = 'block';
+  while (modal.firstChild) {
+    modal.removeChild(modal.firstChild);
+  }
+  modal.style.display = 'none'
+}
+
+// When the user clicks the button, open the modal
+btn.onclick = function () {
+  var flag = false; // if not found id ahow not found
+
+  modal.style.display = 'block';
   var readSearchID = document.getElementById('searchID').value;
   while (modal.firstChild) {
     modal.removeChild(modal.firstChild);
-
   }
-for(i=0;i<objArray.length;i++){
-  console.log(objArray[i].contactID)
-    if(objArray[i].contactID == readSearchID){
-flag = true
-         modal.setAttribute('class', 'contact'); //control individual small box use this class
-          
-          //create id
-          var p2 = document.createElement('p'); //make paragraph tag
-          var text2 = document.createTextNode('ID:' + objArray[i].contactID);
-          p2.appendChild(text2); //put text line 29 to paragraph line 28
-          modal.appendChild(p2); // after that put paragraph on the white box modal
-      
-          //create name last name
-          var p1 = document.createElement('p'); //make paragraph tag
-          var text = document.createTextNode(
-            'name:' + objArray[i].firstName + ' ' + objArray[i].lastName
-          );
-          p1.appendChild(text); //put text line 29 to paragraph line 28
-          modal.appendChild(p1); // after that put paragraph on the white box modal
-      
-      
-          //create address
-          var p3 = document.createElement('p'); //make paragraph tag
-          var text3 = document.createTextNode('Address:' + objArray[i].address);
-          p3.appendChild(text3); //put text line 29 to paragraph line 28
-          modal.appendChild(p3); // after that put paragraph on the white box modal
-      
-          //create phone
-          var p4 = document.createElement('p'); //make paragraph tag
-          var text4 = document.createTextNode('Phone:' + objArray[i].phone);
-          p4.appendChild(text4); //put text line 29 to paragraph line 28
-          modal.appendChild(p4); // after that put paragraph on the white box modal
-      
-          var button = document.createElement('input');
-          button.setAttribute('type', 'button');
-          button.setAttribute('value', 'Delete');
-          button.setAttribute('class', 'btnDelete');
-          button.setAttribute('onclick','deleteObjButton('+objArray[i].contactID + ')')
-          modal.appendChild(button); // put button on small modal box
-      
-        //   document.getElementById('contactList').appendChild(modal); //we add all info small box in line 26 mainbox
-        }  
-    }
-    document.getElementById('searchID').value =''
-    if(flag==false){
+  for (i = 0; i < objArray.length; i++) {
+    console.log(objArray[i].contactID);
+    if (objArray[i].contactID == readSearchID) {
+      flag = true;
       modal.setAttribute('class', 'contact'); //control individual small box use this class
-          
+
+      //create id
       var p2 = document.createElement('p'); //make paragraph tag
-      p2.setAttribute('id', 'tagetNotFoundText')
-      var text2 = document.createTextNode('Contact is not found');
+      var text2 = document.createTextNode('ID:' + objArray[i].contactID);
       p2.appendChild(text2); //put text line 29 to paragraph line 28
       modal.appendChild(p2); // after that put paragraph on the white box modal
-  
-    }
-}
 
+      //create name last name
+      var p1 = document.createElement('p'); //make paragraph tag
+      var text = document.createTextNode(
+        'name:' + objArray[i].firstName + ' ' + objArray[i].lastName
+      );
+      p1.appendChild(text); //put text line 29 to paragraph line 28
+      modal.appendChild(p1); // after that put paragraph on the white box modal
+
+      //create address
+      var p3 = document.createElement('p'); //make paragraph tag
+      var text3 = document.createTextNode('Address:' + objArray[i].address);
+      p3.appendChild(text3); //put text line 29 to paragraph line 28
+      modal.appendChild(p3); // after that put paragraph on the white box modal
+
+      //create phone
+      var p4 = document.createElement('p'); //make paragraph tag
+      var text4 = document.createTextNode('Phone:' + objArray[i].phone);
+      p4.appendChild(text4); //put text line 29 to paragraph line 28
+      modal.appendChild(p4); // after that put paragraph on the white box modal
+
+      var button = document.createElement('input');
+      button.setAttribute('type', 'button');
+      button.setAttribute('value', 'Delete');
+      button.setAttribute('class', 'btnDelete');
+      button.setAttribute(
+        'onclick',
+        'deleteObjButton(' + objArray[i].contactID + ')'
+      );
+      modal.appendChild(button); // put button on small modal box
+
+      //   document.getElementById('contactList').appendChild(modal); //we add all info small box in line 26 mainbox
+    }
+  }
+  document.getElementById('searchID').value = '';
+  if (flag == false) {
+    modal.setAttribute('class', 'contact'); //control individual small box use this class
+
+    var p2 = document.createElement('p'); //make paragraph tag
+    p2.setAttribute('id', 'tagetNotFoundText');
+    var text2 = document.createTextNode('Contact is not found');
+    p2.appendChild(text2); //put text line 29 to paragraph line 28
+    modal.appendChild(p2); // after that put paragraph on the white box modal
+  }
+};
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+span.onclick = function () {
+  modal.style.display = 'none';
+};
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal) {
-    modal.style.display = "none";
+    modal.style.display = 'none';
   }
-}
+};
 
-// searchbox
-function readValueSearchBox(){
 
-}
 // update box
-function updateUserFunc(){
-var updateInfo = document.getElementById('updateUserInfo').value
-for(i=0;i<objArray.length;i++){
-  if(objArray[i].contactID==updateInfo){
-if(document.getElementById('addressID').checked == true){
-objArray[i].address = document.getElementById('fillNewInfo').value//adress line 6
-}else{
-  objArray[i].phone = document.getElementById('fillNewInfo').value//adress line 6
-}
+function updateUserFunc() {
+  var updateInfo = document.getElementById('updateUserInfo').value;
+  for (i = 0; i < objArray.length; i++) {
+    if (objArray[i].contactID == updateInfo) {
+      if (document.getElementById('addressID').checked == true) {
+        objArray[i].address = document.getElementById('fillNewInfo').value; //adress line 6
+      } else {
+        objArray[i].phone = document.getElementById('fillNewInfo').value; //adress line 6
+      }
+    }
   }
+  displayContact();
+  document.getElementById('updateUserInfo').value = ''
 }
-displayContact()
-}
-
-
 
 // Object creation: <!DOCTYPE html>
 
